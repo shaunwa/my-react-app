59394b6672ae1c3dfb40f48ebd4364e945605999
Shaun Wassell <shaun@trove.ai>
2020-06-15 16:20:40 -0400

    Updates

---
+++ precache-manifest.4e00ac6c6f5021cc623a5dc1d621c849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631e81b7280703542bfdee0e258b3730",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "81681b8c2549d00a76df",
+    "url": "/my-react-app/static/css/main.73ecec61.chunk.css"
+  },
+  {
+    "revision": "675873d13d719c221ff7",
+    "url": "/my-react-app/static/js/2.3b4f0078.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.3b4f0078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81681b8c2549d00a76df",
+    "url": "/my-react-app/static/js/main.1b6477a9.chunk.js"
+  },
+  {
+    "revision": "8ab9acae240754f13773",
+    "url": "/my-react-app/static/js/runtime-main.4062a718.js"
+  }
+]);
